48c31e4f5b04aa1c8c39452e41b2a942a2754a9f
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-09-05 09:37:40 -0600

    Create post request for word submission

--- src/javascript/pastedText.js
+++ src/javascript/pastedText.js
@@ -1,12 +1,29 @@
+const baseUrl = 'https://wordwatch-api.herokuapp.com'
+
 class PastedText {
   constructor(){}
 
   listen(){
     $('.text-submission button').on('click', ()=>{
-      let submission = $('.text-submission textarea').val()
-      debugger;
+      let submission = $('.text-submission textarea').val().split(" ");
+      this.postWords(submission)
     })
   }
+
+  postWords(words){
+    words.forEach(singleWord => {
+      let wordParams = { word: { value: singleWord } }
+      $.post(baseUrl + '/api/v1/words', wordParams)
+      .then(function(result){
+        debugger;
+      })
+      .catch(this.handleError);
+    })
+  };
+
+  handleError(error) {
+    console.error(error);
+  };
 }
 
 module.exports = PastedText
